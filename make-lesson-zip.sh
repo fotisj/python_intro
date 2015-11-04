d2bb3e80b5e04ebc215af32127512c8f903028ae
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2015-11-04 10:11:04 +0100

    03 überarbeitet

--- make-lesson-zip.sh
+++ make-lesson-zip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 lesson=`basename $1 .ipynb`
 
-ipython3 nbconvert --to html "$1"
+jupyter nbconvert --to html "$1"
 zip ${lesson}.zip ${lesson}.html `sed -ne 's/^.*img src="\([^"]*\)".*$/\1/p' ${lesson}.html`
